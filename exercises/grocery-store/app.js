2a389a802fbe69cb5d10634f2ada83dd61adaa57
Ryan Pettingill <pettingillryan@gmail.com>
2019-01-25 10:23:24 -0700

    updating

---
+++ exercises/grocery-store/app.js
@@ -0,0 +1,12 @@
+var shopper = {
+    
+    cart: "Walmart Cart",
+    boolean: true,
+    groceryCart: {
+        item1: "picles",
+        intem2: "ham"
+    },
+    fun: function anything(){
+        console.log("catching up right heeerrrreeee")
+    }
+}
