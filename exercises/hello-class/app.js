2074cf32357f3a47838982191961a06b9155cce7
Ryan Pettingill <pettingillryan@gmail.com>
2019-01-24 17:02:28 -0700

    Insert

---
+++ exercises/hello-class/app.js
@@ -0,0 +1,12 @@
+// function sayHello(x,y,z){
+//     console.log("Hello " + x + ", "+ y + " " + z + "!")
+// }
+// let you = "you wonderful";
+// let per = "person";
+// sayHello("Ryan", you, per)
+
+function stateAge(name , age){
+    console.log("Hello" + ' ' + name + ', ' + "how do you like being" + ' ' + age +'!')
+}
+stateAge("Attila", 33)
+stateAge("Ryan" , 24)
