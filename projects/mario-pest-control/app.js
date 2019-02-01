be9be68a554a9f84f33db9520419db687254ef34
Ryan Pettingill <pettingillryan@gmail.com>
2019-02-01 10:32:42 -0700

    Feb. 1st Friday 2019

--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -3,11 +3,17 @@ let goombas = totalCoins.goombas
 let bobOmbs = totalCoins.bobOmbs
 let cheepCheeps = totalCoins.cheepCheeps
 let equal = document.getElementById("equal")
+var img = document.createElement("img");
+var src = document.getElementById("equalImage");
+img.setAttribute("src", "https://www.pngarts.com/files/3/Mario-Coin-PNG-High-Quality-Image.png");
+img.setAttribute("title", "Total Coins");
 totalCoins.addEventListener("submit", function (event) {
     event.preventDefault()
     let x = parseInt(goombas.value)
     let y = parseInt(bobOmbs.value)
     let z = parseInt(cheepCheeps.value)
+    src.appendChild(img);
+    img.className = "img2";
     equal.innerText = ((x * 5) + (y * 7) + (z * 11))
-
+    equal.className = "answer"
 })
