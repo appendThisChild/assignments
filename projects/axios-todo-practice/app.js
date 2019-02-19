4f84c07638f136f8d3e90e3049237534f9418aac
Ryan Pettingill <pettingillryan@gmail.com>
2019-02-19 13:41:32 -0700

    Feb. 19th Tuesday 2019

--- projects/axios-todo-practice/app.js
+++ projects/axios-todo-practice/app.js
@@ -1,9 +1,12 @@
+
 //////////////////
 // Global Variables
 
 const api = "https://api.vschool.io/ryan/todo/";
 const list = document.getElementById('todo-list');
 const form = document["todo-form"];
+const box = document.getElementById("checkBox")
+window.onload = getInfo()
 ///////////////////
 // Functions
 
@@ -14,11 +17,10 @@ function getInfo(){
         insertTodo(todo)
     }).catch(error => console.log(error)) 
 }
-
 // Places the information on the dom in a specific way
 function insertTodo(arr){
     list.innerHTML = ""
-    for (let i = 0; i < arr.length; i++){
+    for (let i = arr.length -1 ; i > -1; i--){
         // Created elements
         const listContainer = document.createElement('div')
         const todoElement = document.createElement('h2')
@@ -27,9 +29,18 @@ function insertTodo(arr){
         const price = document.createElement('p')
         // Element content
         todoElement.textContent = arr[i].title
-        price.textContent = arr[i].price
-        des.textContent = arr[i].description
+        if (arr[i].price === null){
+            price.textContent = `Price: none`
+        } else {
+            price.textContent = `Price: ${arr[i].price}`
+        }
+        if (arr[i].description === ""){
+            des.textContent = `Notes: none`
+        } else {
+            des.textContent = `Notes: ${arr[i].description}`
+        }
         img.setAttribute("src", arr[i].imgUrl)
+        listContainer.id = arr[i]["_id"]
 
         // Setting styles
         img.className = "todoImg"
@@ -44,6 +55,8 @@ function insertTodo(arr){
         listContainer.appendChild(todoElement)
         listContainer.appendChild(price)
         listContainer.appendChild(des)
+        checkBox(listContainer)
+        deleteTodo(listContainer)
         list.appendChild(listContainer)
     } 
 }
@@ -58,4 +71,45 @@ form.addEventListener("submit", (e) => {
     }
     axios.post(api, newTodo).then(res => getInfo()).catch(error => console.log(error))
 })
-getInfo()
+// Making a Checkbox 
+function checkBox(container){
+    let check = document.createElement('input')
+    check.type = "checkbox"
+    // check.id = `checkBox`
+    // check.value = "value"
+    let label = document.createElement('label')
+    label.appendChild(document.createTextNode("Completed: "))
+    container.appendChild(label)
+    container.appendChild(check)
+    // Change function to checkbox
+    check.addEventListener("change", function(e){
+        let id = e.target.parentNode.id
+        let curApi = `${api}${id}`
+        let completed = {
+            completed: true
+        }
+        let uncompleted = {
+            completed: false
+        }
+        axios.get(curApi).then(res => {
+            let complete = res.data.completed
+            if (this.checked && complete === false){
+            axios.put(curApi, completed).then(res => getInfo()).catch(err => console.log(err))
+            } else if (this.checked && complete === true){
+                axios.put(curApi, uncompleted).then(res => getInfo()).catch(err => console.log(err))
+            }
+        }).catch(error => console.log(error)) 
+        
+    })
+}
+function deleteTodo(container){
+    let x = document.createElement("p")
+    x.textContent = "delete"
+    x.className = "delete"
+    container.appendChild(x)
+    x.addEventListener("click", function(e){
+        let id = e.target.parentNode.id
+        let curApi = `${api}${id}`
+        axios.delete(curApi).then(res => getInfo()).catch(err => console.log(err))
+    })
+}
