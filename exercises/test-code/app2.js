cf0e29e06ea6fb06f52cd861a9b4dd700f81ddbc
Ryan Pettingill <pettingillryan@gmail.com>
2020-02-21 18:09:13 -0700

    editing old projects

---
+++ exercises/test-code/app2.js
@@ -0,0 +1,2 @@
+console.log(new Date(1575332256136))
+console.log(new Date(1575508600127))
--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -4,15 +4,15 @@ let bobOmbs = totalCoins.bobOmbs
 let cheepCheeps = totalCoins.cheepCheeps
 let equal = document.getElementById("equal")
 var img = document.createElement("img");
-var src = document.getElementById("equalImage");
-img.setAttribute("src", "https://www.pngarts.com/files/3/Mario-Coin-PNG-High-Quality-Image.png");
+// var src = document.getElementById("equalImage");
+// img.setAttribute("src", "https://www.pngarts.com/files/3/Mario-Coin-PNG-High-Quality-Image.png");
 img.setAttribute("title", "Total Coins");
 totalCoins.addEventListener("submit", function (event) {
     event.preventDefault()
     let x = parseInt(goombas.value)
     let y = parseInt(bobOmbs.value)
     let z = parseInt(cheepCheeps.value)
-    src.appendChild(img);
+    // src.appendChild(img);
     img.className = "img2";
     equal.innerText = ((x * 5) + (y * 7) + (z * 11))
     equal.className = "answer"
